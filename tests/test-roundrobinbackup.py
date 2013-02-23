200d42def62c324556c943e8f01300d04b9b9a35
Chris Laskey <contact@chrislaskey.com>
2013-02-22 19:06:32 -0500

    Added more robust tests using CommandLine mock

--- tests/test-roundrobinbackup.py
+++ tests/test-roundrobinbackup.py
@@ -1,18 +1,21 @@
 # -*- coding: utf8 -*-
 
-# nosetests --with-coverage --cover-package=roundrobinbackupoptionsparser \
+# nosetests --with-coverage --cover-package=roundrobinbackup \
 # --nocapture ./tests
 
 import sys
 from nose.tools import *
 from tests.mocksandstubs import CommandLineMock
 from tests.utils import no_stdout_or_stderr
+from utilities.roundrobindate import RoundRobinDate
 from roundrobinbackup import RoundRobinBackup
 
 class TestRoundRobinBackup:
 
     def setup(self):
         "Set up test fixtures"
+        todays_date = RoundRobinDate().get_today()
+        self.today = todays_date
 
     def teardown(self):
         "Tear down test fixtures"
@@ -102,6 +105,7 @@ class TestRoundRobinBackup:
         cli_input_output = [
             ('ssh user@target.com /bin/mkdir -p /some/path/latest', ''),
             ('rsync -avz -e ssh /local/files user@target.com:/some/path/latest', ''),
+            ('ssh user@target.com /bin/tar -cjf /some/path/automated-backup-{0}.tar.bzip2 /some/path/latest'.format(self.today), ''),
             ('ssh user@target.com /bin/ls /some/path', '')
         ]
         cli_mock = CommandLineMock(cli_input_output)
@@ -119,17 +123,63 @@ class TestRoundRobinBackup:
         # Oldest date will be used as an anchor. The second date does not line
         # up with the anchor date, and should be requested to be removed,
         # generating the last command in cli_input_output.
-        existing_backup_files = [
+        existing_backup_files = [ # Note file's extension doesn't matter
             'automated-backup-1996-01-21',
-            'automated-backup-2004-02-21',
-            'automated-backup-2004-02-22'
+            'automated-backup-2004-02-21.tar.bzip2',
+            'automated-backup-2004-02-22.tar'
         ]
         existing_backups = '\n'.join(existing_backup_files)
         cli_input_output = [
             ('ssh user@target.com /bin/mkdir -p /some/path/latest', ''),
             ('rsync -avz -e ssh /local/files user@target.com:/some/path/latest', ''),
+            ('ssh user@target.com /bin/tar -cjf /some/path/automated-backup-{0}.tar.bzip2 /some/path/latest'.format(self.today), ''),
             ('ssh user@target.com /bin/ls /some/path', existing_backups),
-            ('ssh user@target.com /bin/rm -r /some/path/automated-backup-2004-02-21 /some/path/automated-backup-2004-02-22', '')
+            ('ssh user@target.com /bin/rm -r /some/path/automated-backup-2004-02-21.tar.bzip2 /some/path/automated-backup-2004-02-22.tar', '')
+        ]
+        cli_mock = CommandLineMock(cli_input_output)
+        rrbackup = RoundRobinBackup()
+        rrbackup.set_command_line_library(cli_mock)
+        rrbackup.backup()
+
+    def test_full_execution_with_options_when_remote_archives_exist(self):
+        arguments = [
+            '/local/files',
+            'user@target.com:/some/path',
+            '--exclude',
+            '.git/*',
+            '--exclude',
+            '.venv/*',
+            '--ssh-identity-file',
+            '/dev/null',
+            '--ssh-port',
+            '2222',
+            '--days',
+            '5',
+            '--weeks',
+            '4',
+            '--months',
+            '3',
+            '--years',
+            '2',
+            '--rsync-dir',
+            'live-files',
+            '--backup-prefix',
+            'rrbackup'
+        ]
+        self.set_command_line_arguments(arguments)
+
+        existing_backup_files = [ # Note file's extension doesn't matter
+            'rrbackup1996-01-21',
+            'rrbackup2004-02-21.tar.bzip2',
+            'rrbackup2004-02-22.tar'
+        ]
+        existing_backups = '\n'.join(existing_backup_files)
+        cli_input_output = [
+            ('ssh user@target.com -p 2222 -i /dev/null /bin/mkdir -p /some/path/live-files', ''),
+            ('rsync -avz -e ssh -p 2222 -i /dev/null /local/files user@target.com:/some/path/live-files --exclude .git/* --exclude .venv/*', ''),
+            ('ssh user@target.com -p 2222 -i /dev/null /bin/tar -cjf /some/path/rrbackup{0}.tar.bzip2 /some/path/live-files'.format(self.today), ''),
+            ('ssh user@target.com -p 2222 -i /dev/null /bin/ls /some/path', existing_backups),
+            ('ssh user@target.com -p 2222 -i /dev/null /bin/rm -r /some/path/rrbackup2004-02-21.tar.bzip2 /some/path/rrbackup2004-02-22.tar', '')
         ]
         cli_mock = CommandLineMock(cli_input_output)
         rrbackup = RoundRobinBackup()
